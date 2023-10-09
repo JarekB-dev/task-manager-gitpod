84416be35f460614296dd847266feb27371070f1
Jarek Bonk <jerrybonq@gmail.com>
2023-10-09 19:44:35 +0000

    ..

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
   let sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
+
+
+  // modal
+  let modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {
+      done: "Select"
+    }
+  })
+
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
 });
